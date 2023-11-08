74b04579a9d2e1bada2e9958b96e10fddfb9ea7b
Vladislav Penchuk <vladislavpenchuk@gmail.com>
2023-11-08 11:17:24 -0800

    updated files for linux support

---
+++ linux.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# Load .env file
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found"
+    exit 1
+fi
+
+# New configuration directories under the 'configs' directory
+profile_config_dir="./configs/profiles"
+prompt_config_dir="./configs/prompts"
+
+# Default configuration file names
+default_profile_config="default-profile"
+default_prompt_config="default-prompt"
+
+# Use the first script argument as the profile config file name, or default if none is provided
+profile_config_file="${1:-$default_profile_config}.sh"
+
+# Use the second script argument as the prompt config file name, or default if none is provided
+prompt_config_file="${2:-$default_prompt_config}.txt"
+
+# Check if the profile configuration file exists, otherwise exit with error
+if [ ! -f "$profile_config_dir/$profile_config_file" ]; then
+    echo "Profile configuration file not found: $profile_config_dir/$profile_config_file"
+    exit 1
+fi
+
+# Check if the prompt configuration file exists, otherwise exit with error
+if [ ! -f "$prompt_config_dir/$prompt_config_file" ]; then
+    echo "Prompt configuration file not found: $prompt_config_dir/$prompt_config_file"
+    exit 1
+fi
+
+# Load the profile configuration
+source "$profile_config_dir/$profile_config_file"
+
+# Load the OpenAI script
+source "$(dirname "$0")/openai.sh"
+
+# Load the prompt configuration
+prompt_text=$(cat "$prompt_config_dir/$prompt_config_file")
+
+# Saves directory and custom subfolder path
+saves_dir="./saves"
+custom_dir="${saves_dir}/${custom_subfolder}"
+
+# Current timestamp for the directory or file name
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Decide whether to create a timestamp directory based on the configuration
+if [ "$timestamp_as_dir" = true ]; then
+    custom_dir="${custom_dir}/${timestamp}"
+    aggregate="${custom_dir}/${aggr_file_name}.txt"
+    found_files_list="${custom_dir}/${proj_files_list_name}.txt"
+else
+    aggregate="${custom_dir}/${aggr_file_name}.txt"
+    found_files_list="${custom_dir}/${proj_files_list_name}.txt"
+fi
+
+# Check and create custom subdirectory within saves if not exists
+mkdir -p "$custom_dir"
+
+# Empty the aggregate file if it exists or create a new one
+> "$aggregate"
+
+# Empty the found files list file if it exists or create a new one
+> "$found_files_list"
+
+# Construct the find command as an array
+find_command=( find "$root_dir" -type f )
+
+# Add inclusion patterns for file extensions
+for ext in "${extensions[@]}"; do
+    find_command+=( -iname "*.$ext" )
+done
+
+# Add exclusion patterns for directories and files
+for exclusion in "${exclusions[@]}"; do
+    find_command+=( ! -path "$exclusion" -prune )
+done
+
+# Execute the find command using array expansion to preserve arguments
+while IFS= read -r file; do
+    # Check if the file exists
+    if [ -f "$file" ]; then
+        # Convert POSIX path back to Windows path for output
+        # win_path=$(echo $file | sed 's/\//\\/g' | sed 's/^./\0:/')
+        # Write the file path/name to the aggregate file
+        echo "//$win_path" >> "$aggregate"
+        echo "$win_path" >> "$found_files_list"
+        # Append the file contents to the aggregate file
+        cat "$file" >> "$aggregate"
+        # Add separator lines between each file's content
+        echo -e "\n\n\n" >> "$aggregate"
+    else
+        echo "Error: $file not found."
+    fi
+done < <( "${find_command[@]}" )
+
+echo "Aggregation complete. Check $aggregate for the combined contents."
+
+# Call the summarize function with its contents
+if [ "$summarization" = true ]; then
+    if [ -s "$aggregate" ]; then
+        aggregate_content=$(<"$aggregate")
+        echo "Calling OpenAI API for Summarization with $prompt_config_file - Please wait..."
+        summary_file="${custom_dir}/${summ_file_name}.txt"  # Define the proper summary filename
+        summary=$(summarize_with_prompt "$aggregate_content" "$prompt_text" "$summary_file")  # Pass the summary filename to the function
+        echo "Summary saved to $summary_file"
+    else
+        echo "Aggregate file is empty or does not exist."
+    fi
+else
+    echo "Summarization OFF, No OpenAI Summary."
+fi
--- run.sh
+++ run.sh
@@ -1,102 +1,30 @@
 #!/bin/bash
+
+# Get the operating system type from .host file
 OS=$(<.host)
 
-#[[ $OS = "Linux" ]] && { PORT_CHECK=$(lsof -i:40120); PID=$(lsof -ti:40120); }
-#[[ $OS = "Windows" ]] && { PORT_CHECK=$(netstat -ano | grep "40120" | grep "ESTABLISHED" ); PID=$(netstat -ano | grep "40120" | grep "ESTABLISHED" | awk 'NR==1{ print $5 }'); }
+# Check if the .env file exists and load it
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+else
+    echo ".env file not found"
+    exit 1
+fi
 
+# Here $1 and $2 represent the first and second arguments passed to run.sh
+first_arg=$1
+second_arg=$2
 
-# The list of file paths/names
-files=(
-    #  "../SuperGPT/app.js"
-    #-"../SuperGPT/routes/conversationRoutes.js"
-    #-"../SuperGPT/controllers/conversationController.js"
-
-    #-"../SuperGPT/fileStorage.js"
-    #  "../SuperGPT/gpt4.js"
-
-    #  "../SuperGPT/frontend/src/ChatList.js"
-    #  "../SuperGPT/frontend/src/ChatControls.js"
-    #  "../SuperGPT/frontend/src/Chat.js"
-    #  "../SuperGPT/frontend/src/ChatInput.js"
-
-    #-"../SuperGPT/models/message.js"
-    #-"../SuperGPT/models/conversation.js"
-
-    #-"../SuperGPT/agents/BaseAgent.js"
-    #-"../SuperGPT/agents/AgentManager.js"
-    #  "../SuperGPT/agents/AgentDriver.js"
-    #-"../SuperGPT/agents/ConversationManager.js"
-    #-"../SuperGPT/agents/SpecializedAgents/GenericAgent.js"
-
-    #--------------------------------------------------------
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\models\index.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\models\Twitch_Streamers.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\models\Twitch_Streams.js"
-
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\index.js"
-
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\routes\SearchTwitchID.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\routes\SearchTwitchUsername.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\routes\Twitch_Streamers.js"
-
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\Internal_Scripts\Twitch_getStreamerList.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\Internal_Scripts\Twitch_StreamerUpdates.js"
-
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\Scraping_Scripts\driver.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\Scraping_Scripts\twitchInsert.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\Scraping_Scripts\twitchScrape.js"
-
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\twitch_api\getRateLimit.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\twitch_api\pollLiveIDs.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\twitch_api\pollLiveStreams.js"
-    "D:\Development\TheResearchCorp\MyStats.tv\v3\stream-tracker\server\twitch_api\rateLimiter.js"
-)
-
-[[ $OS = "Linux" ]] && {
-    # The aggregate file
-    aggregate="aggregate.txt"
-
-    # Empty the aggregate file if it exists or create a new one
-    > "$aggregate"
-
-    # Loop through the list of files
-    for file in "${files[@]}"; do
-        # Check if the file exists
-        if [ -f "$file" ]; then
-            # Write the file path/name to the aggregate file without the dot
-            echo "//$file" >> "$aggregate"
-            # Append the file contents to the aggregate file
-            cat "$file" >> "$aggregate"
-            # Add 3 new lines between each file's content
-            echo -e "\n\n\n" >> "$aggregate"
-        else
-            echo "Error: $file not found."
-        fi
-    done
-
-    echo "Aggregation complete. Check $aggregate for the combined contents."
-}
-
-[[ $OS = "Windows" ]] && {
-    aggregate="summary.txt"
-
-    # Empty the aggregate file if it exists or create a new one
-    > "$aggregate"
-
-    # Assuming 'files' is an array, loop through the list of files
-    for file in "${files[@]}"; do
-        # Check if the file exists
-        if [ -f "$file" ]; then
-            # Write the file path/name to the aggregate file without the dot
-            echo "//$file" >> "$aggregate"
-            # Append the file contents to the aggregate file
-            cat "$file" >> "$aggregate"
-            # Add 3 new lines between each file's content
-            echo -e "\n\n\n" >> "$aggregate"
-        else
-            echo "Error: $file not found."
-        fi
-    done
-
-    echo "Aggregation complete. Check $aggregate for the combined contents."
-}
+# Depending on the OS, run the appropriate script and pass the arguments along
+if [[ $OS = "Linux" ]]; then
+    echo "Running linux.sh for $OS"
+    # Pass the arguments to linux.sh
+    ./linux.sh "$first_arg" "$second_arg"
+elif [[ $OS = "Windows" ]]; then
+    echo "Running windows.sh for $OS"
+    # Pass the arguments to windows.sh
+    ./windows.sh "$first_arg" "$second_arg"
+else
+    echo "Unsupported OS: $OS"
+    exit 2
+fi
