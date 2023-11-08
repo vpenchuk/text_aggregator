c0052fe45aa443e5faaca90fab6869f382b13731
Vladislav Penchuk <vladislavpenchuk@gmail.com>
2023-11-07 17:32:15 -0800

    fixed jq argument length
    which caused openAI JSON errors.
    
    Also added summarization flag for profile configs

--- aggr.sh
+++ aggr.sh
@@ -60,31 +60,51 @@ mkdir -p "$custom_dir"
 # Empty the aggregate file if it exists or create a new one
 > "$aggregate"
 
+# function to document / summarize the aggregated file
 summarize_with_openai() {
     local file_content=$1
     local prompt_text=$2
-
+    
     # Your OpenAI API key should be read from an environment variable
     local openai_api_key=$OPENAI_API_KEY
-
+    
     # API endpoint for OpenAI Chat
     local openai_endpoint="https://api.openai.com/v1/chat/completions"
-
-    # Prepare the data for the API call
-    local data=$(jq -n \
-        --arg prompt "$prompt_text" \
-        --arg content "$file_content" \
-        '{model: "gpt-4-1106-preview", messages: [{"role": "system", "content": $prompt}, {"role": "user", "content": $content}]}')
-
+    
+    # Temporary file for the JSON payload
+    local json_payload=$(mktemp)
+    
+    # Temp File for jp slurping
+    echo "$file_content" > /tmp/file_content.tmp
+    
+    # Reformat / sanitize the prompt + context for JSON API call
+    jq -R -s '.' /tmp/file_content.tmp > /tmp/file_content_formatted.tmp
+    
+    #cat /tmp/file_content.tmp
+    
+    # Create the JSON payload
+    jq -n \
+    --arg prompt "$prompt_text" \
+    --slurpfile content /tmp/file_content_formatted.tmp \
+    '{model: "gpt-4-1106-preview", stream: false, messages: [{"role": "system", "content": $prompt}, {"role": "user", "content": ($content | add)}]}' > "$json_payload"
+    
+    #cat "$json_payload"
+    
     # Make the API call and store the response
     local summary_response=$(curl -s -X POST $openai_endpoint \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer $openai_api_key" \
-        -d "$data")
-
+    --data-binary @"$json_payload")
+    
+    # Remove the temporary file
+    rm "$json_payload"
+    # Remove the temporary file
+    rm /tmp/file_content.tmp
+    
+    
     # Extract the text from the response
     local summary=$(echo "$summary_response" | jq -r '.choices[0].message.content')
-
+    
     # Check for errors in the response
     if [[ "$summary" == "" || "$summary" == "null" ]]; then
         local error_message=$(echo "$summary_response" | jq -r '.error.message')
@@ -96,18 +116,19 @@ summarize_with_openai() {
             return 1
         fi
     fi
-
+    
     echo "$summary"
-
+    
     # Save summary to a file
+    local summary_filename
     if [ "$include_timestamp" = true ]; then
-        echo "$summary" > "$custom_dir/$summ_file_name.txt_${timestamp}.txt"
+        summary_filename="$custom_dir/${summ_file_name}_${timestamp}.txt"
     else
-        echo "$summary" > "$custom_dir/$summ_file_name.txt"
+        summary_filename="$custom_dir/${summ_file_name}.txt"
     fi
-    # echo "$summary" > "$custom_dir/$summ_file_name.txt"
-
-    echo "\n\nSummary saved to $custom_dir/$summ_file_name.txt"
+    
+    echo "$summary" > "$summary_filename"
+    echo "Summary saved to $summary_filename"
 }
 
 # Construct the find command as an array
@@ -143,12 +164,16 @@ done < <( "${find_command[@]}" )
 echo "Aggregation complete. Check $aggregate for the combined contents."
 
 # Call the summarize function with its contents
-if [ -s "$aggregate" ]; then
-    aggregate_content=$(<"$aggregate")
-    echo "Calling OpenAI API for Summarization...Please wait..."
-    summary=$(summarize_with_openai "$aggregate_content" "$prompt_text")  # Pass the prompt text to the function
-    echo "Summary:"
-    echo "$summary"
+if [ "summarization" = true ]; then
+    if [ -s "$aggregate" ]; then
+        aggregate_content=$(<"$aggregate")
+        echo "Calling OpenAI API for Summarization...Please wait..."
+        summary=$(summarize_with_openai "$aggregate_content" "$prompt_text")  # Pass the prompt text to the function
+        echo "Summary:"
+        echo "$summary"
+    else
+        echo "Aggregate file is empty or does not exist."
+    fi
 else
-    echo "Aggregate file is empty or does not exist."
+    echo "Summarization OFF"
 fi
