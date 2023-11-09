3501753091143d3b7590b4af86fbf7f1e4839172
Vladislav Penchuk <VladislavPenchuk@gmail.com>
2023-11-08 16:41:25 -0800

    Refactoring OpenAI.sh
    repeated curl code split into functions

--- openai.sh
+++ openai.sh
@@ -1,121 +1,94 @@
+#!/bin/bash
+
 summarize_with_prompt() {
     local file_content=$1
     local prompt_text=$2
-    local summary_filename=$3 # Add a third parameter to accept the summary filename
+    local summary_filename=$3
     local stream_mode=$4
-
     local openai_api_key=$OPENAI_API_KEY
     local openai_endpoint="https://api.openai.com/v1/chat/completions"
 
-    # Temporary file for the JSON payload
-    local json_payload=$(mktemp)
-
-    # Temp File for jp slurping
-    echo "$file_content" >/tmp/file_content.tmp
-
-    # Reformat / sanitize the prompt + context for JSON API call
-    jq -R -s '.' /tmp/file_content.tmp >/tmp/file_content_formatted.tmp
-
-    # Create the JSON payload
-    jq -n \
+    # Create the JSON payload directly from variables
+    local json_payload
+    json_payload=$(jq -n \
         --arg prompt "$prompt_text" \
+        --arg file_content "$file_content" \
         --argjson stream "$stream_mode" \
-        --slurpfile content /tmp/file_content_formatted.tmp \
-        '{model: "gpt-4-1106-preview", stream: $stream, messages: [{"role": "system", "content": $prompt}, {"role": "user", "content": ($content | add)}]}' >"$json_payload"
+        '{
+            model: "gpt-4-1106-preview",
+            stream: $stream,
+            messages: [
+                {"role": "system", "content": $prompt},
+                {"role": "user", "content": $file_content}
+            ]
+        }')
 
-    # Printing the JSON for diagnostic purposes; this can be removed or redirected as needed
-    echo >&2
-    echo "Starting API request with the json_payload" >&2
-    #cat "$json_payload" >&2
-    echo "Please wait, stream_mode: $stream_mode" >&2
-    echo >&2
+    # Initialize or clear the summary file
+    : >"$summary_filename"
 
-    # clear the previous summary file
-    >"$summary_filename"
-
-    echo "API responses:" >&2
-    if [[ "$stream_mode" = true ]]; then
+    # Function to perform the curl request
+    perform_curl() {
         curl -s -X POST "$openai_endpoint" \
             -H "Content-Type: application/json" \
             -H "Authorization: Bearer $openai_api_key" \
-            --data-binary @"$json_payload" |
-            (
+            --data-binary @- <<<"$json_payload"
+    }
 
-                while IFS= read -r line; do
-                    # Remove "data: " prefix before processing the JSON
-                    clean_line=$(echo "$line" | sed 's/^data: //g')
-                    # Extract "content" and "finish_reason" using jq, but ignore stderr output
-                    content=$(echo "$clean_line" | jq -r '.choices[0].delta.content // empty' 2>/dev/null)
-                    finish_reason=$(echo "$clean_line" | jq -r '.choices[0].finish_reason' 2>/dev/null)
+    handle_stream_response() {
+        local line clean_line content finish_reason
 
-                    # Check if content is non-empty before appending
-                    if [[ -n "$content" ]]; then
-                        # Output to console, handling new lines
-                        if [[ "$content" == $'\n' ]]; then
-                            echo "$content" >&2
-                        else
-                            echo -n "$content" >&2
-                        fi
-                        # Append to file, handling new lines
-                        if [[ "$content" == $'\n' ]]; then
-                            echo "$content" >>"$summary_filename"
-                        else
-                            echo -n "$content" >>"$summary_filename"
-                        fi
-                    fi
+        while IFS= read -r line; do
+            # Remove "data: " prefix before processing the JSON
+            clean_line=$(echo "$line" | sed 's/^data: //')
 
-                    # Check for a non-null finish_reason
-                    if [ "$finish_reason" != "null" ] && [ -n "$finish_reason" ]; then
-                        echo >&2
-                        echo >&2
-                        echo "Finish reason: $finish_reason" >&2
-                        break
-                    fi
-                done
-                echo "Summary saved to $summary_filename"
-            )
-    elif [[ "$stream_mode" = false ]]; then
-        local summary_response=$(curl -s -X POST $openai_endpoint \
-            -H "Content-Type: application/json" \
-            -H "Authorization: Bearer $openai_api_key" \
-            --data-binary @"$json_payload")
+            # Extract "content" and "finish_reason" using jq, but ignore stderr output
+            content=$(echo "$clean_line" | jq -r '.choices[0].delta.content // empty' 2>/dev/null)
+            finish_reason=$(echo "$clean_line" | jq -r '.choices[0].finish_reason' 2>/dev/null)
 
-        # Remove the temporary file
-        #rm "$json_payload"
-        # Remove the temporary file
-        #rm /tmp/file_content.tmp
-
-        # Extract the text from the response
-        local summary=$(echo "$summary_response" | jq -r '.choices[0].message.content')
-
-        # Check for errors in the response
-        if [[ "$summary" == "" || "$summary" == "null" ]]; then
-            local error_message=$(echo "$summary_response" | jq -r '.error.message')
-            if [[ "$error_message" != "null" ]]; then
-                echo "Error from OpenAI: $error_message"
-                return 1
-            else
-                echo "No summary was provided or an unknown error occurred."
-                return 1
+            # Check if content is non-empty before appending
+            if [[ -n "$content" ]]; then
+                # Output to console, handling new lines
+                if [[ "$content" == $'\n' ]]; then
+                    echo "$content" >&2
+                else
+                    echo -n "$content" >&2
+                fi
+                # Append to file, handling new lines
+                if [[ "$content" == $'\n' ]]; then
+                    echo "$content" >>"$summary_filename"
+                else
+                    echo -n "$content" >>"$summary_filename"
+                fi
             fi
-        fi
 
-        echo "$summary" >&2
+            # Check for a non-null finish_reason
+            if [[ "$finish_reason" != "null" ]] && [[ -n "$finish_reason" ]]; then
+                echo >&2
+                echo >&2
+                echo "Finish reason: $finish_reason" >&2
+                break
+            fi
+        done
+    }
 
-        # Save summary to a file
-        local summary_filename
-        if [ "$include_timestamp" = true ]; then
-            summary_filename="$custom_dir/${summ_file_name}_${timestamp}.txt"
-        else
-            summary_filename="$custom_dir/${summ_file_name}.txt"
-        fi
-
-        echo "$summary" >"$summary_filename"
-        echo "Summary saved to $summary_filename"
+    # Make API request and handle the response
+    if [[ "$stream_mode" == "true" ]]; then
+        echo "API responses (streaming):" >&2
+        perform_curl | handle_stream_response
     else
-        echo "whoops"
+        local summary_response
+        summary_response=$(perform_curl)
+
+        local summary=$(jq -er '.choices[0].message.content // empty' <<<"$summary_response")
+        if [[ -n "$summary" ]]; then
+            echo "$summary" | tee "$summary_filename" >&2
+        else
+            echo "Error: No summary content received from OpenAI."
+            return 1
+        fi
     fi
 
-    # Clean up temporary files
-    rm "$json_payload" /tmp/file_content.tmp /tmp/file_content_formatted.tmp
+    echo "Summary saved to $summary_filename"
 }
+
+export -f summarize_with_prompt # Export function if running in a subshell
